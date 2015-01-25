da8ff0c0c87706a0536000e6f703f0dd3a8d68ff
gsanta <santagergely90@gmail.com>
2015-01-25 19:17:24 +0100

    list todos from server

--- www/js/controllers/DashCtrl.js
+++ www/js/controllers/DashCtrl.js
@@ -1,11 +1,31 @@
 var DashCtrl = function($scope, $http) {
 
-  $http.post('https://result-estimator.herokuapp.com/sedcup/add', {kurva: 'kurvaAdat'}).success(function(data) {
-      console.log(data.data)
-  })
-  .error(function(data){
-    console.log(data)   
-  });
+	var todoList = [ 
+	  	{
+	  		title: "first todo",
+	  		content: "first todo content",
+	  		date: new Date()
+	  	},
+	  	{
+	  		title: "second todo",
+	  		content: "second todo content",
+	  		date: new Date()
+	  	}
+	]
+
+	// Simple POST request example (passing data) :
+	$http.post('https://result-estimator.herokuapp.com/sedcup/add', {msg:todoList}).
+	  success(function(data, status, headers, config) {
+	    // this callback will be called asynchronously
+	    // when the response is available
+	    $scope.todos = data.body.msg;
+	    //$scope.todos = 
+	 }).
+	 error(function(data, status, headers, config) {
+	    // called asynchronously if an error occurs
+	    // or server returns response with an error status.
+	});
+
 
 }
 
