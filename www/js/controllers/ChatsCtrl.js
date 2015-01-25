a8559145485fa456e229f096fa634ea4674e248d
gsanta <santagergely90@gmail.com>
2015-01-25 21:50:40 +0100

    location info added to Todo

--- www/js/controllers/ChatsCtrl.js
+++ www/js/controllers/ChatsCtrl.js
@@ -4,14 +4,27 @@ var ChatsCtrl = function($scope, $http, $location) {
   $scope.todo = {}
 
   $scope.sendToServer = function() {
-  	$http.post('https://result-estimator.herokuapp.com/todos/add', $scope.todo).
-	  success(function(data, status, headers, config) {
-	    $location.path('/')
-	 }).
-	 error(function(data, status, headers, config) {
-	    // called asynchronously if an error occurs
-	    // or server returns response with an error status.
-	});
+
+
+	  navigator.geolocation.getCurrentPosition(
+	      function(position) {
+	          	console.log(position.coords.latitude + ',' + position.coords.longitude);
+	            $scope.todo.latitude = position.coords.latitude;
+	            $scope.todo.longitude = position.coords.longitude;
+
+	          	$http.post('https://result-estimator.herokuapp.com/todos/add', $scope.todo).
+				  success(function(data, status, headers, config) {
+				    $location.path('/')
+				 }).
+				 error(function(data, status, headers, config) {
+				    // called asynchronously if an error occurs
+				    // or server returns response with an error status.
+				});
+	      },
+	      function() {
+	          alert('Error getting location');
+	      });
+  	
   }
 }
 
