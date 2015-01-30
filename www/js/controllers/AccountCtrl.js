1cf20880dab346051ea08c0e7951eee453ad8975
pataiadam <pataiadam@gmail.com>
2015-01-30 17:06:54 +0100

    asrfg

--- www/js/controllers/AccountCtrl.js
+++ www/js/controllers/AccountCtrl.js
@@ -1,16 +1,42 @@
 
-var AccountCtrl = function($scope) {
+var AccountCtrl = function($scope, $location) {
   $scope.settings = {
     enableFriends: true
   };
 
-  $scope.marker = null;
+    $scope.data = {
+        radius: 0,
+        startPos: {}
+    };
+
+
+    $scope.marker = null;
 
     var mapProp = {
 	    center:new google.maps.LatLng(51.508742,-0.120850),
 	    zoom:5,
 	    mapTypeId:google.maps.MapTypeId.ROADMAP
 	  };
+
+
+    $scope.setMission = function() {
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                console.log("new"+position.coords.latitude + ',' + position.coords.longitude);
+                $scope.data.startPos.latitude = position.coords.latitude;
+                $scope.data.startPos.longitude = position.coords.longitude;
+
+                window.startPos = {
+                    lat: $scope.data.startPos.latitude,
+                    lon: $scope.data.startPos.longitude
+                }
+                window.destPos = $scope.customPos
+                $location.path('/tab/dash');
+            },
+            function() {
+                alert('Error getting location');
+            }, {timeout: 15000, enableHighAccuracy: false});
+    }
 
   	$scope.initMap = function() {
 
@@ -22,15 +48,15 @@ var AccountCtrl = function($scope) {
 			      position: event.latLng,
 			      map: $scope.map,
 			      title: 'Hello World!',
-			      draggable:true,
+			      draggable:true
 			  });
 
 			  	google.maps.event.addListener($scope.marker, 'dragend', function() 
 				{
-				    console.log({
+				    $scope.customPos = {
 				    	lat: $scope.marker.getPosition().lat(),
 				    	lon: $scope.marker.getPosition().lng()
-				    })
+				    }
 				});
 
 		  	}
--- www/js/controllers/FriendsCtrl.js
+++ www/js/controllers/FriendsCtrl.js
@@ -1,16 +1,67 @@
 
-var FriendsCtrl = function($scope) {
+var FriendsCtrl = function($scope, $location) {
 
     $scope.data = {
-        radius: 0
+        radius: 0,
+        startPos: {}
     };
 
     $scope.setMission = function(rad) {
         if(rad===undefined){
             rad=$scope.data.radius;
         }
+        $scope.data.radius = rad;
         console.log(rad);
+
+        initMission()
     }
+
+    $scope.toMap = function() {
+        console.log("lefdafsddf")
+        $location.path('/tab/account');
+    }
+
+    var initMission = function() {
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                console.log("new"+position.coords.latitude + ',' + position.coords.longitude);
+                $scope.data.startPos.latitude = position.coords.latitude;
+                $scope.data.startPos.longitude = position.coords.longitude;
+
+                window.startPos = {
+                    lat: $scope.data.startPos.latitude,
+                    lon: $scope.data.startPos.longitude
+                }
+                window.destPos = generateRandomCoord($scope.data.startPos.latitude, $scope.data.startPos.longitude, $scope.data.radius);
+
+                $location.path('/tab/dash');
+            },
+            function() {
+                alert('Error getting location');
+            }, {timeout: 15000, enableHighAccuracy: false});
+    }
+
+    var generateRandomCoord = function(lat, lon, radius) {
+        var r = radius/111300 // = 100 meters
+            , y0 = lat
+            , x0 = lon
+            , u = Math.random()
+            , v = Math.random()
+            , w = r * Math.sqrt(u)
+            , t = 2 * Math.PI * v
+            , x = w * Math.cos(t)
+            , y1 = w * Math.sin(t)
+            , x1 = x / Math.cos(y0)
+
+        newY = y0 + y1
+        newX = x0 + x1
+        return {
+            lat: newY,
+            lon: newX
+        }
+    }
+
+    window.generateCoord = generateRandomCoord;
 
 }
 
