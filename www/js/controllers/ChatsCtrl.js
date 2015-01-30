92deb4702a5d74640862621eac2aaccdf566e6d3
pataiadam <pataiadam@gmail.com>
2015-01-30 15:31:59 +0100

    get diff between 2 coords

--- www/js/controllers/ChatsCtrl.js
+++ www/js/controllers/ChatsCtrl.js
@@ -1,5 +1,5 @@
 
-var ChatsCtrl = function($scope, $http, $location, $window) {
+var ChatsCtrl = function($scope, $http, $location) {
   
   $scope.todo = {}
 
@@ -27,6 +27,67 @@ var ChatsCtrl = function($scope, $http, $location, $window) {
 	      }, {timeout: 15000, enableHighAccuracy: false});
   	
   }
+
+    /*
+        start = {
+            latitude: x
+            longitude: y
+        }
+     goal = {
+        latitude: x
+        longitude: y
+     }
+     */
+    window.getDiff = function(start, goal) {
+
+
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                console.log("new"+position.coords.latitude + ',' + position.coords.longitude);
+
+                var current = {
+                    latitude: position.coords.latitude,
+                    longitude:position.coords.longitude
+                }
+
+                var fulldiff= getdifcoord(start, goal)*(3/2);
+                var currentdif = getdifcoord(current, goal);
+                var koz = fulldiff-currentdif;
+
+                console.log(koz/fulldiff);
+                return koz/fulldiff;
+
+            },
+            function() {
+                alert('Error getting location');
+            }, {timeout: 15000, enableHighAccuracy: false});
+
+    }
+
+    var getdifcoord = function(a, b){
+        if (typeof(Number.prototype.toRad) === "undefined") {
+            Number.prototype.toRad = function() {
+                return this * Math.PI / 180;
+            }
+        }
+        var lat1 = a.latitude;
+        var lon1 =a.longitude;
+        var lat2 = b.latitude;
+        var lon2 =b.longitude;
+        var R = 6371; // km
+        var φ1 = lat1.toRad();
+        var φ2 = lat2.toRad();
+        var Δφ = (lat2-lat1).toRad();
+        var Δλ = (lon2-lon1).toRad();
+
+        var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+       return R * c;
+    }
+
 }
 
 module.exports = ChatsCtrl
