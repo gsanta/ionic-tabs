54e467e9e03522e1652f5f561aaeaa7e40e03268
student <student@student-iMac.(none)>
2015-01-30 14:45:44 +0100

    First commit :D, Custom mission selector added

--- www/js/controllers/FriendsCtrl.js
+++ www/js/controllers/FriendsCtrl.js
@@ -1,6 +1,15 @@
 
-var FriendsCtrl = function($scope, Friends) {
-  $scope.friends = Friends.all();
+var FriendsCtrl = function($scope) {
+
+    $scope.data = {};
+
+    $scope.setMission = function(rad) {
+        if(rad===undefined){
+            rad=$scope.data.radius;
+        }
+        console.log(rad);
+    }
+
 }
 
 module.exports = FriendsCtrl
